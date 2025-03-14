4979b0d06f338c3ecdfbe308290bf99f8afd5dfc
tinkared <tinkared@gmail.com>
2025-03-14 12:19:15 +0100

    array added to 2.ts

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -3,13 +3,17 @@ type AllType = {
     position: number;
     color: string;
     weight: number;
+};
+
+function compare<T extends keyof AllType>(
+    top: Pick<AllType, T>,
+    bottom: Pick<AllType, T>
+): [AllType, [string, number]] {
+    const result: AllType = {
+    name: top.name as string,
+    color: top.color as string,
+    position: bottom.position as number,
+    weight: bottom.weight as number,
+    };
 }
 
-function compare<T extends keyof AllType>(top: Pick<AllType, T>, bottom: Pick<AllType, T>): AllType {
-    return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-    }
-}
